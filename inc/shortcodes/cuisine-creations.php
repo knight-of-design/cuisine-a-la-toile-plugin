aaa7c875e720a1766196770d02b7021dd3d31e99
James Knight <knight.of.design@gmail.com>
2016-03-24 10:01:33 -0400

    Styling

--- inc/shortcodes/cuisine-creations.php
+++ inc/shortcodes/cuisine-creations.php
@@ -1,8 +1,9 @@
 <?php
-if ( !function_exists('cuisine_plugin_shortcode__cuisine_creations' ) and !shortcode_exists('cuisine-creations')) {
-    function cuisine_plugin_shortcode__cuisine_creations($options, $content = null){
+if (!function_exists('cuisine_plugin_shortcode__cuisine_creations') and !shortcode_exists('cuisine-creations')) {
+    function cuisine_plugin_shortcode__cuisine_creations($options, $content = null)
+    {
         // Copy shortcode options to local variables
-        $default_options =  array(
+        $default_options = array(
             'numposts' => 1,
             'order' => 'DESC',
             'mealcourse' => '',
@@ -11,34 +12,43 @@ if ( !function_exists('cuisine_plugin_shortcode__cuisine_creations' ) and !short
 
         $shortcode_options = shortcode_atts($default_options, $options);
 
+        $meta_query = null;
+        if ($shortcode_options['mealcourse']) {
+            $meta_query = array(
+                'relation' => 'AND',
+                array(
+                    'key' => 'Chef Username',
+                    'value' => array('savoryalways'),
+                    'compare' => 'IN',
+                )
+            );
+        }
+
+        $tax_query = null;
+        if ($shortcode_options['chefusername']) {
+            $tax_query = array(
+                'relation' => 'AND',
+                array(
+                    'taxonomy' => 'cuisine_creation_meal_course',
+                    'field' => 'slug',
+                    'terms' => 'entree',//$shortcode_options['mealcourse']
+                )
+            );
+        }
         $creations_options = array(
             'content' => $content,
             'query' => array(
                 'posts_per_page' => $shortcode_options['numposts'],
                 'order' => $shortcode_options['order'],
-                'tax_query' => array(
-                    'relation'		=> 'AND',
-                  array(
-                    'taxonomy' => 'cuisine_creation_meal_course',
-                    'field' => 'slug',
-                    'terms' => $shortcode_options['mealcourse']
-                  )
-                ),
-                'meta_query'	=> array(
-                    'relation'		=> 'AND',
-                    array(
-                        'key'	 	=> 'Chef Username',
-                        'value'	  	=> array('savoryalways'),
-                        'compare' 	=> 'IN',
-                    )
-                )
+                'tax_query' => $tax_query,
+                'meta_query' => $meta_query
             )
         );
         // Render shortcode
-        return cuisine_template_html('shortcodes/cuisine-creations',$creations_options);
+        return cuisine_template_html('shortcodes/cuisine-creations', $creations_options);
 
     }
 
-    add_shortcode('cuisine-creations','cuisine_plugin_shortcode__cuisine_creations');
+    add_shortcode('cuisine-creations', 'cuisine_plugin_shortcode__cuisine_creations');
 }
 ?>
